47124325901f6c05f0f15eba133d40d149677d91
mfbil <mfbil@MFBILGIN>
2022-08-24 12:13:10 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Domain/Entites/Brand.cs
@@ -0,0 +1,24 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entites
+{
+    public class Brand:Entity
+    {
+        public string Name { get; set; }
+
+        public Brand()
+        {
+        }
+
+        public Brand(int id,string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -0,0 +1,28 @@
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+//builder.Services.AddApplicationServices();
+//builder.Services.AddSecurityServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
+//builder.Services.AddInfrastructureServices();
+//builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); 
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
